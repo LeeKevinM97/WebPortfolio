e278e4c116d118d37a5ac9bd85ed1446738b3827
Lee, Kevin <kevinmlee97@gmail.com>
2023-06-12 16:37:05 -0500

    Start Initial React App

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+        <div className="h-full">
+            <p className="text-7xl">
+                Hello
+            </p>
+            Header
+        </div>
+    </>
+  )
+}
+
+export default Header;
