18e8a092103c974bb271feb3c97fb57daebd1e6a
Lee, Kevin <kevinmlee97@gmail.com>
2023-07-31 09:26:13 -0500

    Generate Border

---
+++ src/components/Border.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Border() {
+  return (
+    <div>Border</div>
+  )
+}
+
+export default Border
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <div className="bg-first h-fit w-full">
+      <h1 className="text-4xl text-center font-semibold mb-6 text-fourth roboto">Follow Me!</h1>
+    </div>
+      
+    </>
   )
 }
 
