317f32112e3d5bc12fcd8ace37d5ea66cfc9abb9
Lee, Kevin <kevinmlee97@gmail.com>
2023-09-01 09:26:31 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,11 @@ function Footer() {
     <Border/>
     <div className="bg-first h-fit w-full">
       <h1 className="text-4xl text-center font-semibold mb-6 text-fourth roboto">Follow Me!</h1>
-      <div className='flex justify-center pb-12'>
-        <a href="https://www.linkedin.com/in/leekevinmaximiliano/" target="_blank" className="bg-second w-fit h-fit mx-4 p-2 rounded-xl hover:p-3">
+      <div className='flex justify-center'>
+        <a href="https://www.linkedin.com/in/leekevinmaximiliano/" target="_blank" className="bg-second w-fit h-fit mx-4 p-2 rounded-xl hover:p-3 mb-12 hover:mb-9">
           <FontAwesomeIcon icon={faLinkedin} className="text-white h-10 w-10"/>
         </a>
-        <a href="https://github.com/leekevinm97/" target="_blank" className="bg-second w-fit h-fit mx-4 p-2 rounded-xl hover:p-3">
+        <a href="https://github.com/leekevinm97/" target="_blank" className="bg-second w-fit h-fit mx-4 p-2 rounded-xl hover:p-3 mb-12 hover:mb-9">
           <FontAwesomeIcon icon={faGithub} className="text-white h-10 w-10"/>
         </a>
       </div>
