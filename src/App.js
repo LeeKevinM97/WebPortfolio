ca7a7980943c8f9ac4cd923cc303f01d298c45b8
Lee, Kevin <kevinmlee97@gmail.com>
2023-09-27 16:58:31 -0500

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
@@ -14,7 +14,7 @@ import './css/Main.css';
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header/>
       <Routes>
         <Route index element={<Home/>}/>
@@ -24,7 +24,7 @@ function App() {
         <Route path="contact" element={<ContactForm/>}/>
       </Routes>
       <Footer/>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
