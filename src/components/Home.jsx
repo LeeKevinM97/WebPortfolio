c86adfe3f8fc2a48ce680ac7424ee17a5d615b4f
Lee, Kevin <kevinmlee97@gmail.com>
2023-09-27 18:04:53 -0500

    Updated Skills Section
    -

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,30 @@ import React from "react";
 import Border from "../components/Border.jsx";
 
 function Home() {
+  const skillsData = [
+    { name: 'Python', value: 100 },
+    { name: 'JavaScript', value: 70 },
+    { name: 'HTML', value: 80 },
+    { name: 'CSS', value: 80 },
+    { name: 'Tailwind', value: 85 },
+    { name: 'Unity', value: 50 },
+    { name: 'C#', value: 50 },
+    { name: 'AWS', value: 60 },
+    { name: 'SQL Server MS', value: 40 },
+    { name: 'Docker', value: 20 },
+    { name: 'GitHub', value: 100 },
+    { name: 'React JS', value: 65 },
+    { name: 'ChatGPT', value: 90 },
+    { name: 'SQL', value: 70 },
+  ];
+
+  const sortedSkills = [...skillsData].sort((a, b) => b.value - a.value);
+
   return (
     <>
       <div className="bg-first w-full h-fit flex flex-col lg:flex-row justify-around">
+
+        {/* Portion for Opener and Image */}
         <div className="lg:w-1/2 lg:h-[48rem] flex-auto w-full flex items-center h-[12rem]">
           <div className="text-center w-full">
             <p className="text-fourth font-bold text-4xl my-10 roboto px-10">
@@ -15,14 +36,39 @@ function Home() {
             </p>
           </div>
         </div>
-        <div className="lg:w-1/2 lg:h-[48rem] flex p-10 w-full h-auto flex my-auto mx-auto justify-center align-center">
+        <div className="lg:w-1/2 lg:h-[48rem] flex lg:pr-24 p-8 w-full h-auto flex my-auto mx-auto justify-center align-center">
           <img
-            className="md:h-[32rem] w-auto md:my-auto rounded-[128px] h-auto my-10"
+            className="w-auto md:my-auto rounded-[128px] h-auto my-10"
             src={require("../images/Picture.jpeg")}
             alt="Kevin's Image"
           ></img>
         </div>
       </div>
+
+      <Border />
+
+      {/* Skills Portion */}
+      <div className="bg-first w-full p-4">
+        <h2 className="text-xl sm:text-2xl md:text-3xl mb-10 text-center text-fourth scp">Skills</h2>
+        <div className="flex justify-center">
+          <div className="flex flex-wrap justify-around gap-2 sm:gap-4"> {/* Adjusted for wrapping and spacing */}
+            {sortedSkills.map(skill => (
+              <div key={skill.name} className="text-center w-20 sm:w-24 md:w-28 mb-4"> {/* Added margin-bottom for spacing between rows */}
+                {/* Outer div representing max height */}
+                <div className="h-[96px] w-8 sm:w-10 md:w-12 bg-third relative mx-auto rounded">
+                  {/* Inner div representing skill value */}
+                  <div
+                    className="absolute bottom-0 bg-second transition-all duration-500 ease-in-out w-full rounded"
+                    style={{ height: `${Math.min(skill.value, 100) * 0.96}px` }}
+                  ></div>
+                </div>
+                <p className="mt-2 text-xs sm:text-sm md:text-base text-center truncate text-fourth scp">{skill.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
     </>
   );
 }
